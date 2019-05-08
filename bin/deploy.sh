f009fbd10b34d02c39765ea6a9df59ce0452c134
Brian Grant <brian.grant@urbanairship.com>
2019-05-08 12:15:04 -0700

    update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,7 +7,7 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$TRAVIS_BRANCH" == "master" ]; then
       docker tag  hackoregoncivic/backend-docker-django-dev hackoregoncivic/backend-docker-django
       docker push hackoregoncivic/backend-docker-django
-    elif [ "$TRAVIS_BRANCH" == "staging" ];
+    elif [ "$TRAVIS_BRANCH" == "staging" ]; then
       docker push hackoregoncivic/backend-docker-django-dev
     else
       echo "Skipping deploy because branch is not master or test-deploy branch"
